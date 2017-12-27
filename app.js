e686fab58419f5f5f7777d69ad40f7556ab3b98e
Mykhailo Ivankiv <neformal.lviv@gmail.com>
2017-12-27 20:02:12 +0200

    Fix port #2

--- app.js
+++ app.js
@@ -3,5 +3,4 @@ const app = express()
 
 app
     .use(express.static(__dirname + '/frontend'))
-    .listen(8000, () => console.log('Example app listening on port 3000!'))
-
+    .listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
