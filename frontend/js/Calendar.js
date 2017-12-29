c3731668f0dba5adde208dfc43dd69b7d40fe4ab
Mykhailo Ivankiv <neformal.lviv@gmail.com>
2017-12-30 01:36:16 +0200

    Update schedule.

--- frontend/js/Calendar.js
+++ frontend/js/Calendar.js
@@ -1,3 +1,42 @@
+import Select from "react-select";
+
+const students = [
+  { id: 0, name: "Dmytro Kushnir" },
+  { id: 1, name: "Kovtun Matt" },
+  { id: 2, name: "Maria Dobko" },
+  { id: 3, name: "Maryana Mysak" },
+  { id: 4, name: "Orest Rehusevych" },
+  { id: 5, name: "Pankiv Andriy" },
+  { id: 6, name: "Serhii Dubovyk" },
+  { id: 7, name: "Taras Zelyk" },
+  { id: 8, name: "Anton Tarasov" },
+  { id: 9, name: "Vladyslav Velychko" },
+  { id: 10, name: "Anastasia Vedernikova" },
+  { id: 11, name: "Arsen Senkivskyy" },
+  { id: 12, name: "Andriy Borovets" },
+  { id: 13, name: "Bogdan Kovalchuk" },
+  { id: 14, name: "Dzvinka Yarish" },
+  { id: 15, name: "Korabliova Olya" },
+  { id: 16, name: "Korduba Yaryna" },
+  { id: 17, name: "Laba Yuriy" },
+  { id: 18, name: "Lebedenko Roksoliana" },
+  { id: 19, name: "Marc Soumoussou Kodjovi" },
+  { id: 20, name: "Mohylevska Kateryna" },
+  { id: 21, name: "Myroslav Syvokhip" },
+  { id: 22, name: "Natalija Paranyak" },
+  { id: 23, name: "Nina Bondar" },
+  { id: 24, name: "Oleh Smolkin" },
+  { id: 25, name: "Oleksandr Pryhoda" },
+  { id: 26, name: "Olenyuk Oksana" },
+  { id: 27, name: "Olya Bakay" },
+  { id: 28, name: "Ostap Kharysh" },
+  { id: 29, name: "Pavlyshyn Yaryna" },
+  { id: 30, name: "Petro Karabyn" },
+  { id: 31, name: "Petryshak" },
+  { id: 32, name: "Skibinska Olena" },
+  { id: 33, name: "Yasya Shpot" }
+];
+
 import React, { Component } from "react";
 import { DateTime, Interval } from "luxon";
 import BEM from "./BEM";
@@ -28,14 +67,11 @@ class Calendar extends Component {
     const className = reservation ? "Hour Hour_reserved" : "Hour";
 
     return (
-      <div
-        className={className}
-        onClick={!reservation && this.openScheduler.bind(this, hour)}
-      >
+      <div className={className}>
         <span className="Hour__time">
           {hour.start.toLocaleString(DateTime.TIME_24_SIMPLE)}
         </span>
-        {reservation && reservation.student}
+        {reservation ? reservation.student : ""}
       </div>
     );
   }
@@ -53,6 +89,9 @@ class Calendar extends Component {
 
     return (
       <time className={b("day", { weekend: isWeekend || isHoliday })}>
+        {/*<Select*/}
+          {/*options={students.map(({ id, name }) => ({ value: id, label: name }))}*/}
+        {/*/>*/}
         <span className={b("title")}>
           {start.toLocaleString({
             weekday: "short",
@@ -111,14 +150,21 @@ class Calendar extends Component {
       time: Interval.after(DateTime.local(2018, 1, 3, 12), { hour: 1 })
     },
     {
-        student: "Pankiv Andriy",
-        time: Interval.after(DateTime.local(2018, 1, 3, 14), { hour: 1 })
+      student: "Pankiv Andriy",
+      time: Interval.after(DateTime.local(2018, 1, 3, 14), { hour: 1 })
     },
     {
-        student: "Maria Dobko",
-        time: Interval.after(DateTime.local(2018, 1, 8, 12), { hour: 1 })
+      student: "Maria Dobko",
+      time: Interval.after(DateTime.local(2018, 1, 8, 12), { hour: 1 })
+    },
+    {
+        student: "Olya Bakay",
+        time: Interval.after(DateTime.local(2018, 1, 5, 12), { hour: 1 })
+    },
+    {
+        student: "Skibinska Olena",
+        time: Interval.after(DateTime.local(2018, 1, 1, 12), { hour: 1 })
     }
   ];
 }
-
 export default Calendar;
