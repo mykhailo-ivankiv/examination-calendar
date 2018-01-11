9e2962be25a4adb7b9f3e6d90c93693790849b22
Mykhailo Ivankiv <neformal.lviv@gmail.com>
2018-01-11 23:35:50 +0200

    Update settings.

--- backend/auth.js
+++ backend/auth.js
@@ -4,17 +4,21 @@ const GitHubStrategy = require("passport-github2").Strategy;
 
 const GITHUB_CLIENT_ID = "c248798633e9dcc6d002";
 const GITHUB_CLIENT_SECRET = "ade727b69c01e6a3ee9046823a8fd39db4e9d60e";
-
+const CALBACK_URL =
+  "https://examination-calendar.herokuapp.com/auth/github/callback";
 const ADMIN_ID = "96634";
+
 const users = {};
 
 passport.serializeUser((user, done) => {
-    users[user.id] = user._json;
-    if (user.id === ADMIN_ID) { users[user.id].admin = true; }
-    done(null, user.id)
+  users[user.id] = user._json;
+  if (user.id === ADMIN_ID) {
+    users[user.id].admin = true;
+  }
+  done(null, user.id);
 });
 passport.deserializeUser((obj, done) => {
-    done(null, users[obj])
+  done(null, users[obj]);
 });
 
 passport.use(
@@ -22,7 +26,7 @@ passport.use(
     {
       clientID: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/github/callback"
+      callbackURL: CALBACK_URL
     },
     async (accessToken, refreshToken, profile, done) => done(null, profile)
   )
@@ -45,4 +49,3 @@ router.get("/logout", function(req, res) {
 });
 
 module.exports = router;
-
